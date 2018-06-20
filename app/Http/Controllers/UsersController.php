62b844331ca67aa7febd2e0d656f06923ef290f4
Kirameki Hanako <hanako@example.com>
2018-06-20 06:28:45 +0000

    want

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use \App\Item;
+use App\Http\Controllers\Controller;
 
-class ItemsController extends Controller
+use App\User;
+use App\Item;
+
+class UsersController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -23,35 +26,9 @@ class ItemsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-     public function create()
+    public function create()
     {
-        $keyword = request()->keyword;
-        $items = [];
-        if ($keyword) {
-            $client = new \RakutenRws_Client();
-            $client->setApplicationId(env('RAKUTEN_APPLICATION_ID'));
-
-            $rws_response = $client->execute('IchibaItemSearch', [
-                'keyword' => $keyword,
-                'imageFlag' => 1,
-                'hits' => 20,
-            ]);
-
-            // Creating "Item" instance to make it easy to handle.（not saving）
-            foreach ($rws_response->getData()['Items'] as $rws_item) {
-                $item = new Item();
-                $item->code = $rws_item['Item']['itemCode'];
-                $item->name = $rws_item['Item']['itemName'];
-                $item->url = $rws_item['Item']['itemUrl'];
-                $item->image_url = str_replace('?_ex=128x128', '', $rws_item['Item']['mediumImageUrls'][0]['imageUrl']);
-                $items[] = $item;
-            }
-        }
-
-        return view('items.create', [
-            'keyword' => $keyword,
-            'items' => $items,
-        ]);
+        //
     }
 
     /**
@@ -73,13 +50,16 @@ class ItemsController extends Controller
      */
     public function show($id)
     {
-        $item = Item::find($id);
-      $want_users = $item->want_users;
+        $user = User::find($id);
+        $count_want = $user->want_items()->count();
+        $items = \DB::table('items')->join('item_user', 'items.id', '=', 'item_user.item_id')->select('items.*')->where('item_user.user_id', $user->id)->distinct()->paginate(20);
 
-      return view('items.show', [
-          'item' => $item,
-          'want_users' => $want_users,
-      ]);
+        return view('users.show', [
+            'user' => $user,
+            'items' => $items,
+            'count_want' => $count_want,
+            // 'count_have' => $count_have,
+        ]);
     }
 
     /**
